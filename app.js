032c21113c8e6326f8590c7bdd8214cee6d7ff2b
unknown <1042651820@qq.com>
2018-03-07 09:24:10 +0800

    加入建表语句

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const { loadMiddlewares } = require('./middlewares');
 const { loadRouters } = require('./routes');
 const { runPre } = require('./services/MyChatUtils');
 const socketio = require('./services/socket.io');
+const { usersModel,attributeModel,friendModel,roleModel,user_friendModel,users_attributeModel } = require('./models');
+const { MyChatError } = require('./services/MyChatUtils');
 (async function bootstrap() {
   try {
     await runPre.testMysql();
@@ -17,7 +19,17 @@ const socketio = require('./services/socket.io');
     });
     server.listen(3000);
     console.log(`服务端程序正在监听3000端口`);
+    await createDataBase();
   } catch (err) {
     console.log(err);
   }
 })();
+
+async function createDataBase() {
+    usersModel.createUserTable();
+    attributeModel.createAttributeTable();
+    friendModel.createFriendTable();
+    roleModel.createRoleTable();
+    user_friendModel.createUserFriendTable();
+    users_attributeModel.createUsersAttributeTable();
+}
