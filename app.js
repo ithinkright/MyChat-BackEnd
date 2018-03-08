9c31fd8706810231e7a107d72748af9801f8e8ff
unknown <1042651820@qq.com>
2018-03-08 17:06:45 +0800

    自动建表

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const { runPre } = require('./services/MyChatUtils');
 const socketio = require('./services/socket.io');
 const { usersModel,attributeModel,friendModel,roleModel,user_friendModel,users_attributeModel } = require('./models');
 const { MyChatError } = require('./services/MyChatUtils');
+const queryDB = require('./services/db');
+const compute = require('./services/Compute/index');
+const translate = require('./services/Translator/youdao');
+const { getOriginFrends } = require('./controllers/friends');
 (async function bootstrap() {
   try {
     await runPre.testMysql();
@@ -20,16 +24,24 @@ const { MyChatError } = require('./services/MyChatUtils');
     server.listen(3000);
     console.log(`服务端程序正在监听3000端口`);
     await createDataBase();
+    //await getOriginFrends("21633e8138c5669902a4bed40ec6516b");
+    //await testSql();
   } catch (err) {
     console.log(err);
   }
 })();
 
 async function createDataBase() {
-    usersModel.createUserTable();
-    attributeModel.createAttributeTable();
-    friendModel.createFriendTable();
-    roleModel.createRoleTable();
-    user_friendModel.createUserFriendTable();
-    users_attributeModel.createUsersAttributeTable();
+    await usersModel.createUserTable();
+    await attributeModel.createAttributeTable();
+    await friendModel.createFriendTable();
+    await roleModel.createRoleTable();
+    await user_friendModel.createUserFriendTable();
+    await users_attributeModel.createUsersAttributeTable();
+}
+
+async function testSql() {
+  let obj = {friendid: 8};
+  let [friend] = await friendModel.findFriendById(obj);
+  console.log(friend);
 }
