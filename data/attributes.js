d18418742dc94e489943fd7029a8d51a83df1c2b
unknown <1042651820@qq.com>
2018-03-08 16:44:46 +0800

    默认数据

---
+++ data/attributes.js
@@ -0,0 +1,19 @@
+
+data = [
+  {
+      name: "translate",
+      description: "translate",
+      usercount: "6666"
+  },
+  {
+      name: "compute",
+      description: "compute",
+      usercount: "6666"
+  }
+]
+
+
+
+exports = module.exports = {
+    data
+}
---
+++ data/roles.js
@@ -0,0 +1,21 @@
+
+data = [
+  {
+      name: "translator",
+      description: "translate",
+      attribute: "translate attributeid",
+      usercount: "6666"
+  },
+  {
+      name: "computer",
+      description: "compute",
+      attribute: "compute attributeid",
+      usercount: "6666"
+  }
+]
+
+
+
+exports = module.exports = {
+    data
+}
