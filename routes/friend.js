12dc8294bb6665bbcc606f4d6efd869a3149dd10
unknown <1042651820@qq.com>
2018-03-08 16:56:00 +0800

    添加路由

--- routes/friend.js
+++ routes/friend.js
@@ -3,9 +3,7 @@ const { friendCtrl } = require('../controllers')
 
 const router = new Router({ prefix: '/api/friends' });
 
-router.get('/', async function (ctx, next) {
-  ctx.body = 'this is a friends api route'
-})
+router.get('/', friendCtrl.getFriends)
 
 router.post('/', friendCtrl.addFriend)
 
