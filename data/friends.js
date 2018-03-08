fc61ebc845608aead4ff10335c67bf9b0a52da59
unknown <1042651820@qq.com>
2018-03-08 13:12:02 +0800

    添加部分假数据做测试用

---
+++ data/friends.js
@@ -0,0 +1,33 @@
+
+data = [
+  {
+      friendname: "computer",
+      gender: "male",
+      birth: "20170908",
+      roleid: "df53ca268240ca76670c8566ee54568a"
+  },
+  {
+      friendname: "translator",
+      gender: "female",
+      birth: "20090908",
+      roleid: "607f2f3099f2a347b327caa70e0be4b2"
+  },
+  {
+      friendname: "Email",
+      gender: "female",
+      birth: "20030101",
+      roleid: "ce8ae9da5b7cd6c3df2929543a9af92d"
+  },
+  {
+      friendname: "Secure",
+      gender: "female",
+      birth: "20180308",
+      roleid: "1c0b76fce779f78f51be339c49445c49"
+  }
+]
+
+
+
+exports = module.exports = {
+    data
+}
---
+++ data/index.js
@@ -0,0 +1,5 @@
+exports = module.exports = {
+    friendsData: require('./friends'),
+    attributesData: require('./attributes'),
+    rolesData: require('./roles')
+}
