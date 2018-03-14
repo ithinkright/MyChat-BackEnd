d53360ac49615cadef4ab58fa28b4e3f56e63a01
unknown <1042651820@qq.com>
2018-03-14 12:27:14 +0800

    bug

--- controllers/dealMessage.js
+++ controllers/dealMessage.js
@@ -10,37 +10,31 @@ async function process(ctx, next) {
     let [friend] = await friendModel.findFriendById({ friendid: obj.friendid });
     if (!friend) {
         throw new MyChatError(2, '该朋友不存在');
-        sendRes(ctx, {result: result});
     }
     let choice = friend.attribute.split(',')[0];
     let [attribute] = await attributeModel.findAttributeById({ attributeid: choice });
-    try {
-        switch (attribute.name) {
-          case "compute":
-            try {
-                result = compute(obj.mes);
-            } catch (e) {
-                throw new MyChatError(2, "输入的字符串无法求值");
-                result = "无法求值，请升级为VIP";
-            }
-            result = result.toString();
-            break;
-          case "translate":
-            obj.from = "auto";
-            obj.to = "ja";
-            obj.query = obj.mes;
-            let res = await translate(obj);
-            result = res.translation[0];
-            break;
+    if (!attribute) {
+        throw new MyChatError(2, '该属性不存在');
+    }
+    switch (attribute.name) {
+      case "compute":
+        try {
+          result = compute(obj.mes);
         }
+        catch (e) {
+          sendRes(ctx, {result: "该字符串无法求值"})
+        }
+        result = result.toString();
+        break;
+      case "translate":
+        obj.from = "auto";
+        obj.to = "ja";
+        obj.query = obj.mes;
+        let res = await translate(obj);
+        result = res.translation[0];
+        break;
     }
-    catch (e) {
-        console.log(e);
-        throw new MyChatError(2, "属性不存在")
-    }
-    finally {
-      sendRes(ctx, {result: result});
-    }
+    sendRes(ctx, {result: result});
 }
 
 exports = module.exports = {
