12060075e78f4bb51b5a7a9a94abcfcfb73fed70
unknown <1042651820@qq.com>
2018-04-13 19:14:40 +0800

    天气bug

--- controllers/weather.js
+++ controllers/weather.js
@@ -15,23 +15,28 @@ async function get(place, next) {
 }
 
 function stringifyWeather(obj) {
-  if (obj.status) {
-    return "城市有误，请重新输入";
+  try {
+      if (obj.status) {
+        return "城市有误，请重新输入";
+      }
+      let str = "";
+      let keyArray = {
+        city: "城市",
+        wendu: "温度",
+        fengli: "风力",
+        shidu: "湿度",
+        fengxiang: "风向"
+      }
+      for (key in keyArray) {
+        str += keyArray[key] + ": " + obj[key] + "\n";
+      }
+      return str;
   }
-  let str = "";
-  let keyArray = {
-    city: "城市",
-    wendu: "温度",
-    fengli: "风力",
-    shidu: "湿度",
-    fengxiang: "风向",
-    sunrise_1: "日升时间",
-    sunset_1: "日落时间"
+  catch(e) {
+    console.log(e);
+    return "输入有误";
   }
-  for (key in keyArray) {
-    str += keyArray[key] + ": " + obj[key] + "\n";
-  }
-  return str;
+  return "输入有误";
 }
 
 exports = module.exports = {
