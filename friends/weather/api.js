2adb1ae268d1cf81194ea45aa637b381751552d3
unknown <1042651820@qq.com>
2018-04-13 19:18:07 +0800

    修复天气bug

--- friends/weather/api.js
+++ friends/weather/api.js
@@ -28,33 +28,29 @@ async function getWeather(place) {
 }
 
 function stringifyWeather(obj) {
-  if (obj.status) {
-    return "城市有误，请重新输入";
-  }
-  let str = "";
-  let keyArray = {
-    city: "城市",
-    wendu: "温度",
-    fengli: "风力",
-    shidu: "湿度",
-    fengxiang: "风向",
-    sunrise_1: "日升时间",
-    sunset_1: "日落时间"
-  }
-  for (key in keyArray) {
-    str += keyArray[key] + ": " + obj[key] + "\n";
-  }
-  str += "空气质量：" + obj.environment.quality + "\n";
-  str += "出行建议：" + obj.environment.suggest + "\n";
-  str += "未来六天预报：\n";
-  let forecastweather = obj.forecast.weather;
-  for (key in forecastweather) {
-    let data = forecastweather[key];
-    str += data.date + "\t";
-    str += data.high + "\t" + data.low + "\t";
-    str += "白天：" + data.day.type + "\t" + "晚上：" + data.night.type + "\n";
-  }
-  return str;
+    try {
+        if (obj.status) {
+          return "城市有误，请重新输入";
+        }
+        let str = "";
+        let keyArray = {
+          city: "城市",
+          wendu: "温度",
+          fengli: "风力",
+          shidu: "湿度",
+          fengxiang: "风向",
+          sunrise_1: "日升时间",
+          sunset_1: "日落时间"
+        }
+        for (key in keyArray) {
+          str += keyArray[key] + ": " + obj[key] + "\n";
+        }
+        return str;
+    }
+    catch(e) {
+      console.log(e);
+      return "输入有误";
+    }
 }
 
 function isToday(date) {
